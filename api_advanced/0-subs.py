7d643920a932a3a6eb962f0f3377fb4a6021e218
gershnsenga <156787203+gershnsenga@users.noreply.github.com>
2024-07-06 21:40:43 +0200

    Update 0-subs.py

--- api_advanced/0-subs.py
+++ api_advanced/0-subs.py
@@ -9,13 +9,16 @@ def number_of_subscribers(subreddit):
     """
     number of subscribers
     """
-    url = "https://oauth.reddit.com/r/{}/about.json".format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}  # avoid Too Many Requests error
+    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    headers = {
+        'User-Agent': 'MyBot/1.0 (by /u/gershom)'
+    }
 
-    response = requests.get(url, headers=headers, allow_redirects=False)
-
-    if response.status_code == 200:
-        data = response.json()
-        return data['data']['subscribers']
-    else:
+    try:
+        response = requests.get(url, headers=headers, allow_redirects=False)
+        if response.status_code == 200:
+            data = response.json()
+            return data['data']['subscribers']
+        return 0
+    except Exception:
         return 0
